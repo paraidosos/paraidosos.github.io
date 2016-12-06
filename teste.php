f196fb23e5a25d12e3521020043fb96b14e545a7
PAULO RICARDO RUBE DE ALMEIDA <paulo@actio.net.br>
2016-12-05 22:19:37 -0200

    Create teste.php

---
+++ teste.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Acessibilidade para idosos</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+        <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <div class="container">
+       <h1 class="text-center">Em breve novidades</h1>
+    </div>
+    <?php
+    echo "hello its me";
+    ?>
+</body>
+</html>
